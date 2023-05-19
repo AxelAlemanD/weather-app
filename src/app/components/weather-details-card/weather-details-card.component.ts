492d87178c5652b05f3ec5ba3374abf0cbc05c04
AxelAlemanD <axelad38@gmail.com>
2023-05-19 16:09:08 -0600

    refactor: Show icon conditionally

--- src/app/components/weather-details-card/weather-details-card.component.ts
+++ src/app/components/weather-details-card/weather-details-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-details-card',
   templateUrl: './weather-details-card.component.html',
   styleUrls: ['./weather-details-card.component.scss']
 })
-export class WeatherDetailsCardComponent {
+export class WeatherDetailsCardComponent implements OnInit {
+
+  @Input() showIcons: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
