6f29045531d52683f3e1b93d60c03b375cce85af
AxelAlemanD <axelad38@gmail.com>
2023-05-19 16:39:10 -0600

    add: Current Weather component

---
+++ src/app/components/current-weather/current-weather.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.scss']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,15 +7,18 @@ import { DailyForecastCardComponent } from '../components/daily-forecast-card/da
 import { HourlyForecastCardComponent } from '../components/hourly-forecast-card/hourly-forecast-card.component';
 import { WeatherDetailsCardComponent } from '../components/weather-details-card/weather-details-card.component';
 import { TemperatureScaleSwitchComponent } from '../components/temperature-scale-switch/temperature-scale-switch.component';
+import { CurrentWeatherComponent } from '../components/current-weather/current-weather.component';
 
 @NgModule({
   declarations: [
+    CurrentWeatherComponent,
     DailyForecastCardComponent,
     HourlyForecastCardComponent,
     WeatherDetailsCardComponent,
     TemperatureScaleSwitchComponent
   ],
   exports: [
+    CurrentWeatherComponent,
     DailyForecastCardComponent,
     HourlyForecastCardComponent,
     WeatherDetailsCardComponent,
