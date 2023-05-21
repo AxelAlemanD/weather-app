e82d6eaca6beddd1b35601c8a0b429eafd852a4a
AxelAlemanD <axelad38@gmail.com>
2023-05-21 11:57:34 -0600

    add: Transition when switching displayed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule, BrowserAnimationsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,17 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({opacity: 0})),
+      state('*', style({opacity: 1})),
+      transition('void <=> *', animate('250ms ease-in-out'))
+    ])
+  ]
 })
 export class HomePage {
 
