be5313510ca132c1e71157e20430a186edef4f65
AxelAlemanD <axelad38@gmail.com>
2023-05-22 10:30:41 -0600

    add: Using Variables to Display Components

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,20 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   animations: [
     trigger('fadeInOut', [
-      state('void', style({opacity: 0})),
-      state('*', style({opacity: 1})),
+      state('void', style({ opacity: 0 })),
+      state('*', style({ opacity: 1 })),
       transition('void <=> *', animate('250ms ease-in-out'))
     ])
   ]
 })
 export class HomePage {
 
+  currentSlide: number = 0;
   showCitySearchResults: boolean = false;
+  cities = ['city 1', 'city 2', 'city 3'];
+  @ViewChild('swiper') swiperRef: ElementRef | undefined;
 
   constructor() { }
 
   toggleCitySearchResultsComponent(event: any) {
     this.showCitySearchResults = (event.type === "ionFocus" && event.type !== "ionCancel");
+  }
+
+  updateCurrentSlide() {
+    this.currentSlide = this.swiperRef?.nativeElement.swiper.activeIndex;
   }
 
 }
