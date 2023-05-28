7257410b2021cc7b58dd18bbea1588af25bd4527
AxelAlemanD <axelad38@gmail.com>
2023-05-28 15:47:01 -0600

    add: Weather model

---
+++ src/app/models/weather.dto.ts
@@ -0,0 +1,13 @@
+export interface Weather {
+    lat: number;
+    lon: number;
+    main: string;
+    description: string;
+    temp: number;
+    max_temp: number;
+    min_temp: number;
+    humidity: number;
+    rain: number;
+    wind: number;
+    updated_at: string;
+}
