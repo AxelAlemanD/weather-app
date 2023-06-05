47b505913d44d03d5b3bd137a30fc90738cc2051
AxelAlemanD <axelad38@gmail.com>
2023-06-05 08:12:34 -0600

    fix: Internet connection in Android app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api_key: '1b268298156ff78033eae50d17555ad1',
-  geolocation_url: 'http://api.openweathermap.org/geo/1.0/',
+  geolocation_url: 'https://api.openweathermap.org/geo/1.0/',
   weather_url: 'https://api.openweathermap.org/data/2.5/'
 };
